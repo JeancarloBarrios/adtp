8b981e9600cd0f10b9bde8302e51a36594e5145a
Jeancarlo Barrios <jeancarlobarrios@gmail.com>
2015-11-05 02:02:43 +0000

    test

--- acuaponic/medd/views.py
+++ acuaponic/medd/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
+
+def display_medicine(request):
+    medicina = med.objects.all()
+
+    return render(request, '', {'medicina': medicina})
+
+def medicine_search(request):
+    search = request.POST['search']
+
+    if (search ==""):
+        medicina = med.objects.all()
+    else:
+        medcine = med.objects.filter(nombre=search)
+    return render(request, '', {'search': search, 'medicina': medicina})
